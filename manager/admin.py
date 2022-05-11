8cea39eaca71dc3423f6674a0c38263009b4e734
Deformater <muravenkog@mail.ru>
2022-05-11 23:58:38 +0300

    feat: django-admin
    add admin for Categories, Lectures, Conferences
    
    feat: add django-mptt

--- manager/admin.py
+++ manager/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from mptt.admin import DraggableMPTTAdmin
+from manager.models import Category, Conference, Lecture
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(DraggableMPTTAdmin):
+    mptt_indent_field = "title"
+    list_display = ('tree_actions', 'indented_title', 'description')
+    list_display_links = ('indented_title', )
+
+
+@admin.register(Conference)
+class ConferenceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'date')
+    list_display_links = ('title', )
+
+
+@admin.register(Lecture)
+class LectureAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
+    list_display_links = ('title', )
--- manager/models.py
+++ manager/models.py
@@ -1,13 +1,29 @@
 from django.db import models
+from django.db.models import Q
 from core.models import TitleDescriptionMixin
 from django.core.files.storage import FileSystemStorage
+from mptt.models import MPTTModel, TreeForeignKey
 
 
 storage = FileSystemStorage(location='/media/file')
 
 
-class Category(TitleDescriptionMixin):
-    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='subcategories')
+class Category(MPTTModel, TitleDescriptionMixin):
+    parent = TreeForeignKey(
+        'self',
+        verbose_name='Родительская категория',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='subcategories'
+    )
+
+    def __str__(self):
+        return self.title[:20]
+
+    class MPTTMeta:
+        level_attr = 'mptt_level'
+        order_insertion_by = ['title']
 
     class Meta:
         verbose_name = 'Категория'
@@ -16,8 +32,16 @@ class Category(TitleDescriptionMixin):
 
 class Conference(TitleDescriptionMixin):
     date = models.DateField('Дата проведения')
-    category = models.ForeignKey(Category, verbose_name='Категории', on_delete=models.SET_NULL,
-                                 related_name='conferences', null=True)
+    category = models.ForeignKey(
+        Category,
+        verbose_name='Категории',
+        on_delete=models.SET_NULL,
+        related_name='conferences',
+        null=True
+    )
+
+    def __str__(self):
+        return self.title[:20]
 
     class Meta:
         verbose_name = 'Конференция'
@@ -25,12 +49,35 @@ class Conference(TitleDescriptionMixin):
 
 
 class Lecture(TitleDescriptionMixin):
-    desired_time = models.DateTimeField('Желаемое время', null=True, blank=True)
-    file = models.FileField('Презентация', storage=storage)
-    conference = models.ForeignKey(Conference, verbose_name='Лекции', on_delete=models.CASCADE, related_name='lectures')
-    category = models.ForeignKey(Category, verbose_name='Категории', on_delete=models.SET_NULL,
-                                 related_name='lectures', null=True)
+    desired_time = models.DateTimeField(
+        'Желаемое время',
+        null=True,
+        blank=True
+    )
+    file = models.FileField(
+        'Презентация',
+        storage=storage
+    )
+    conference = models.ForeignKey(
+        Conference,
+        verbose_name='Конференция',
+        on_delete=models.CASCADE,
+        related_name='lectures'
+    )
+    category = models.ForeignKey(
+        Category,
+        verbose_name='Категории',
+        on_delete=models.SET_NULL,
+        related_name='lectures',
+        null=True
+    )
+
+    def __str__(self):
+        return self.title[:20]
 
     class Meta:
         verbose_name = 'Доклад'
-        verbose_name_plural = 'Доклады'
+        # verbose_name_plural = 'Доклады'
+        # constraints = [
+        #     models.CheckConstraint(check=Q(category__parent=Conference.category), name='age_gte_18')
+        # ]
