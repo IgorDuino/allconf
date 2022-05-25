12a5ff875ce6b1c058ea178e3617efa69247e76c
igor <igor.kuzmenkov.05@mail.ru>
2022-05-26 00:54:24 +0300

    minor typos

--- homepage/views.py
+++ homepage/views.py
@@ -12,12 +12,11 @@ class SearchView(View):
     template_name = 'homepage/search.html'
     
     def get(self, request):
-        lectures = Lecture.objects.filter(is_active=True, confirmed=True).select_related('category')
         conference = Conference.objects.filter(is_active=True).select_related('category')
         
         search_text = request.GET.get('search', '')
         
-        search_list = list(lectures) + list(conference)
+        search_list = list(conference)
         search_list = process.extract(search_text, search_list, limit=len(search_list))
         search_list.sort(key=lambda x: x[1], reverse=True)
         search_list = list(map(lambda x: x[0], search_list))
